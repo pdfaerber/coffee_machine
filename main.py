51b23231f55be30425d54c8017b16765eb3fcfda
Patrick D Faerber <patrickdfaerber@gmail.com>
2024-01-29 11:52:29 -0500

    Update avail_resources function with loop and conditional logic

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print(logo)
 
 
 
-# TODO 2. Print a report of all coffee machine resources
+# TODO 2. Print a report of all coffee machine resources and amount in till
 def print_resources():
     print(f"Available Resources:\n"
           f"Water in ml: {resources.get('water')}\n"
@@ -15,30 +15,51 @@ def print_resources():
           f"Matcha in ml: {resources.get('matcha')}\n"
           f"Amount in Register: ${bank}\n")
 
-espresso = menu.get("espresso")
-latte = menu.get("latte")
-cappuccino = menu.get("cappuccino")
-matcha = menu.get("matcha_latte")
+def avail_resources(drink):
+    for item in drink:
+        if drink[item] >= resources[item]:
+            print(drink[item])
+            print(f'Sorry there is not enough {item}.')
+        else:
+            print(f'Your {drink} is coming right up!')
+
+
+# espresso = menu["espresso"]
+# latte = menu["latte"]
+# cappuccino = menu["cappuccino"]
+# matcha = menu["matcha_latte"]
 
 # keep track of amount in register
 bank = 0
 
 machine = True
 while machine:
-    choice = int(input("Welcome to the pantry! What would you like to choice? \n"
+    choice = int(input("Welcome to the pantry! What drink would you like to choose? \n"
                       "Espresso = 1 / Latte = 2 / Cappuccino = 3 / Matcha = 4: "))
     if choice == 1:
+        espresso = menu["espresso"]
+        avail_resources(espresso['ingredients'])
         print(f"Espresso ingredients are: {espresso['ingredients']} and it cost ${espresso['cost']}")
-        print_resources()
+        print(espresso['ingredients'])
     elif choice == 2:
-        print(f"Latte ingredients are: {latte.get('ingredients')} and it cost ${latte.get('cost')}")
+        latte = menu["latte"]
+        avail_resources(latte['ingredients'])
+        print(f"Latte ingredients are: {latte['ingredients']} and it cost ${latte['cost']}")
         print_resources()
     elif choice == 3:
-        print(f"Cappuccino ingredients are: {cappuccino.get('ingredients')} and it cost ${cappuccino.get('cost')}")
+        cappuccino = menu["cappuccino"]
+        avail_resources(cappuccino['ingredients'])
+        print(f"Cappuccino ingredients are: {cappuccino['ingredients']} and it cost ${cappuccino['cost']}")
         print_resources()
     elif choice == 4:
-        print(f"Matcha Latte ingredients are: {matcha.get('ingredients')} and it cost ${matcha.get('cost')}")
+        matcha = menu["matcha_latte"]
+        avail_resources(matcha['ingredients'])
+        print(f"Matcha Latte ingredients are: {matcha['ingredients']} and it cost ${matcha['cost']}")
         print_resources()
+    # printing resources or shutting down machine
+    elif choice == 5:
+        print_resources()
+
     elif choice == 0:
         print("I'm shutting down now. See Ya!")
         machine = False
